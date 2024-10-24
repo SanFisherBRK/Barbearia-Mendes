458c96a4c2c0d679496bd3d4a65c798553d5dd38
Souza <sanfisher@msn.com>
2024-10-23 23:21:15 -0300

    Alterando o README

--- README.md
+++ README.md
@@ -1,82 +1,114 @@
-# BarbeariaCarlos
+# BarbeariaCarlos 
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/devsuperior/sds1-wmazoni/blob/master/LICENSE) 
+
+# Sobre o projeto
+
+BarbeariaMendes  é uma aplicação full stack desktop construída durante o 4ª semestre do Curso analise e desenvolvimento de sistemas organizado pela Faculdade Impacta.
+
+A aplicação consiste em um software desktop feito para uma barbearia que serve para cadastrar os cliente, serviços, barbeiros e realizar agendamentos por data e hora.
 
 
+## Requisitos do projeto
+- Java 17
+- JavaFX
+- MySql
+- Eclipse
 
-### Sobre o projeto
+## Layout web
 
-
-
-BarbeariaCarlos  é uma aplicação full stack desktop construída durante o 4ª semestre do **Curso analise e desenvolvimento de sistemas **organizado pela Faculdade Impacta.
-
-### Layout
-
-
-
-### Modelo conceitual
-
+<p align="center">
+    <img width="800" height="600" src="https://github.com/SanFisherBRK/Dashboard-feita-em-React-para-a-Api-vendinha/blob/main/src/ImagensGitHub/Layout2.gif"
+</p>
 
 
-## Tecnologias utilizadas
+## Diagrama de classes
 
-### Back end
+<p align="center">
+    <img width="800" height="600" src="https://github.com/SanFisherBRK/Dashboard-feita-em-React-para-a-Api-vendinha/blob/main/src/ImagensGitHub/Layout2.gif"
+</p>
 
-- Java
-- MySQL Drive
+# Tecnologias utilizadas
 
-### Front end
-
+## Front end
 - JavaFX
 
-### Implantação em produção
-
-- Back end: Java
-- Front end JavaFX:
+## Implantação em produção
+- Back end: Java Desktop
+- Front end JavaFx Desktop
 - Banco de dados: Mysql
 
-## Como executar o projeto
 
-### Back end
+## Front end
+Pré-requisitos: Java 17
 
+#Crie as tabelas do banco de dados.
 
+create table usuario(
+	idusuario INT auto_increment primary Key unique not null,
+    nome varchar(255) not null,
+	email varchar(255) not null,
+	login varchar(45) not null,
+	senha varchar(255) not null
+);
 
-Pré-requisitos: Java 11
+create table cliente(
+	idcliente INT auto_increment primary Key unique not null,
+    nome varchar(255) not null,
+	email varchar(255) not null,
+	telefone varchar(45) not null,
+	endereco varchar(255) not null,
+	cpf varchar(11) not null unique,
+	usuario_id int,
+	foreign key (usuario_id) references usuario(idusuario)
+);
 
+create table barbeiro(
+	idbarbeiro INT auto_increment primary Key unique not null,
+    nome varchar(255) not null,
+	email varchar(255) not null,
+	telefone varchar(45) not null,
+	endereco varchar(255) not null,
+	cpf varchar(11) not null unique,
+	usuario_id int,
+	foreign key (usuario_id) references usuario(idusuario)
+);
+
+create table servico(
+	idservico INT auto_increment primary Key unique not null,
+    nome varchar(255) not null,
+	descricao varchar(255) not null,
+	preco DECIMAL(10,2) NOT NULL,
+	usuario_id int,
+	foreign key (usuario_id) references usuario(idusuario)
+);
+
+create table agendamento(
+	idagendamento INT auto_increment primary Key unique not null,
+    datahora TIMESTAMP,
+	cliente_id int,
+	servico_id int,
+	barbeiro_id int,
+	foreign key (cliente_id) references cliente(idcliente),
+    foreign key (servico_id) references servico(idservico),
+    foreign key (barbeiro_id) references barbeiro(idbarbeiro)
+);
+
+```bash
+# clonar repositório
+git clone git@github.com:SanFisherBRK/Barbearia-Mendes.git
+
+# Importar o projeto no eclipse
+
+# criar uma pasta java-libs em c:
+Baixar o conector mysql-connector-j-9.0.0.jar e colocar dentro da pasta java-libs.
+Baixar o javafx-sdk-22.0.2 e colocar dentro da pasta java-libs.
+Configurar o javafx-sdk-22.0.2 e mysql-connector-j-9.0.0.jar no seu projeto dentro do eclipse.
+
+# Como executar o projeto
+Clicar em Run Main para executar o projetos.
 ```
-# Em construção
-git clone git@github.com:SanFisherBRK/Projeto-barbearia.git
 
-# Em construção
-
-
-# Em construção
-```
-
-
-
-### Front end web
-
-
-
-Pré-requisitos: npm / yarn
-
-```
-# Em construção
-
-
-# Em construção
-
-
-# Em construção
-
-
-# Em construção
-```
-
-
-
-### Autor
-
-
+# Autor
 
 Francinaldo e Silva Souza.
 
--- src/gui/BarbeiroViewController.java
+++ src/gui/BarbeiroViewController.java
@@ -14,94 +14,98 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
 public class BarbeiroViewController {
-    // Criar uma variável do tipo Barbeiros
-    Barbeiros barbeiro = new Barbeiros();
-    
-    // Cria as variáveis do tipo TextField que é os campos onde você digita as informações
-    @FXML
-    private TextField txt_nome;
+	// Criar uma variável do tipo Barbeiros
+	Barbeiros barbeiro = new Barbeiros();
 
-    @FXML
-    private TextField txt_email;
+	// Cria as variáveis do tipo TextField que é os campos onde você digita as
+	// informações
+	@FXML
+	private TextField txt_nome;
 
-    @FXML
-    private TextField txt_telefone;
+	@FXML
+	private TextField txt_email;
 
-    @FXML
-    private TextField txt_endereco;
+	@FXML
+	private TextField txt_telefone;
 
-    @FXML
-    private TextField txt_cpf;
+	@FXML
+	private TextField txt_endereco;
 
-    @FXML
-    private Button btn_Cadastrar;
+	@FXML
+	private TextField txt_cpf;
 
-    @FXML
-    private Button btn_limparCampos;
+	@FXML
+	private Button btn_Cadastrar;
 
-    // Faz com que as variáveis da classe Cliente receba nelas o que foi digitado nos campos do formulario.
-    public void cadastrarBarbeiros() throws SQLException {
-        barbeiro.setNome(txt_nome.getText());
-        barbeiro.setEmail(txt_email.getText());
-        barbeiro.setTelefone(txt_telefone.getText());
-        barbeiro.setEndereco(txt_endereco.getText());
-        barbeiro.setCpf(txt_cpf.getText());
+	@FXML
+	private Button btn_limparCampos;
 
-        // Cria a query de inserção dos dados na tabela do banco de dados.
-        String sqlCheckCadastro = "SELECT COUNT(*) FROM barbeiro WHERE cpf = ?";
-        String sql = "INSERT INTO barbeiro (nome, email, telefone, endereco, cpf) VALUES(?, ?, ?, ?, ?)";
+	// Faz com que as variáveis da classe Cliente receba nelas o que foi digitado
+	// nos campos do formulario.
+	public void cadastrarBarbeiros() throws SQLException {
+		barbeiro.setNome(txt_nome.getText());
+		barbeiro.setEmail(txt_email.getText());
+		barbeiro.setTelefone(txt_telefone.getText());
+		barbeiro.setEndereco(txt_endereco.getText());
+		barbeiro.setCpf(txt_cpf.getText());
 
-        // Cria a conexão com o banco de dados dentro de um tratamento de erro
-        try (Connection conn = new Conexao().getConnection();
-             PreparedStatement checarCadastro = conn.prepareStatement(sqlCheckCadastro);
-             PreparedStatement enviaParaOBanco = conn.prepareStatement(sql)) {
+		// Cria a query de inserção dos dados na tabela do banco de dados.
+		String sqlCheckCadastro = "SELECT COUNT(*) FROM barbeiro WHERE cpf = ?";
+		String sql = "INSERT INTO barbeiro (nome, email, telefone, endereco, cpf) VALUES(?, ?, ?, ?, ?)";
 
-            checarCadastro.setString(1, barbeiro.getCpf());
-            ResultSet result = checarCadastro.executeQuery();
-            result.next();
+		// Cria a conexão com o banco de dados dentro de um tratamento de erro
+		try (Connection conn = new Conexao().getConnection();
+				PreparedStatement checarCadastro = conn.prepareStatement(sqlCheckCadastro);
+				PreparedStatement enviaParaOBanco = conn.prepareStatement(sql)) {
 
-            // Faz a verificação para ver se todos os campos foram preenchidos
-            if (barbeiro.getNome().isEmpty() || barbeiro.getEmail().isEmpty() || barbeiro.getTelefone().isEmpty()
-                    || barbeiro.getEndereco().isEmpty() || barbeiro.getCpf().isEmpty()) {
+			checarCadastro.setString(1, barbeiro.getCpf());
+			ResultSet result = checarCadastro.executeQuery();
+			result.next();
 
-                Alerts.showAlert("Cadastrar Cliente", "Preencha todos os campos", "Tentar de novo", AlertType.INFORMATION);
+			// Faz a verificação para ver se todos os campos foram preenchidos
+			if (barbeiro.getNome().isEmpty() || barbeiro.getEmail().isEmpty() || barbeiro.getTelefone().isEmpty()
+					|| barbeiro.getEndereco().isEmpty() || barbeiro.getCpf().isEmpty()) {
 
-            } else {
+				Alerts.showAlert("Cadastrar Cliente", "Preencha todos os campos", "Tentar de novo",
+						AlertType.INFORMATION);
 
-                int count = result.getInt(1);
+			} else {
 
-                if (count > 0) {
+				int count = result.getInt(1);
 
-                    Alerts.showAlert("Cadastrar Barbeiro", "Cadastro já existe", "ok", AlertType.INFORMATION);
+				if (count > 0) {
 
-                } else {
+					Alerts.showAlert("Cadastrar Barbeiro", "Cadastro já existe", "ok", AlertType.INFORMATION);
 
-                    // Envia os dados digitados para a tabela cliente no banco de dados barbearia
-                    enviaParaOBanco.setString(1, barbeiro.getNome());
-                    enviaParaOBanco.setString(2, barbeiro.getEmail());
-                    enviaParaOBanco.setString(3, barbeiro.getTelefone());
-                    enviaParaOBanco.setString(4, barbeiro.getEndereco());
-                    enviaParaOBanco.setString(5, barbeiro.getCpf());
+				} else {
 
-                    enviaParaOBanco.executeUpdate();
+					// Envia os dados digitados para a tabela cliente no banco de dados barbearia
+					enviaParaOBanco.setString(1, barbeiro.getNome());
+					enviaParaOBanco.setString(2, barbeiro.getEmail());
+					enviaParaOBanco.setString(3, barbeiro.getTelefone());
+					enviaParaOBanco.setString(4, barbeiro.getEndereco());
+					enviaParaOBanco.setString(5, barbeiro.getCpf());
 
-                    Alerts.showAlert("Cadastrar Cliente", "Cadastro realizado com sucesso", "ok", AlertType.INFORMATION);
-                }
-            }
+					enviaParaOBanco.executeUpdate();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+					Alerts.showAlert("Cadastrar Cliente", "Cadastro realizado com sucesso", "ok",
+							AlertType.INFORMATION);
+				}
+			}
 
-    @FXML
-    public void limparCampos() {
-        txt_nome.clear();
-        txt_email.clear();
-        txt_telefone.clear();
-        txt_endereco.clear();
-        txt_cpf.clear();
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void limparCampos() {
+		txt_nome.clear();
+		txt_email.clear();
+		txt_telefone.clear();
+		txt_endereco.clear();
+		txt_cpf.clear();
+	}
 }
